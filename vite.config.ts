3c5c2b82d4e4f62ceb43d829dd8b84d4d5066ea7
Michael Mano <michael.mano26@gmail.com>
2025-09-13 11:17:06 +1000

    Fix up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-// vite.config.ts
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
-import { defineConfig as defineVitestConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
-// Export Vite + Vitest config
-export default defineVitestConfig({
+export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: '/espanol-verbos/',
   test: {
-    base: '/espanol-verbos/',
-    environment: 'jsdom',
-    globals: true,
+    environment: 'jsdom', // allows DOM APIs in tests
+    globals: true, // enables describe/it/test without imports
     setupFiles: './vitest.setup.ts',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
     },
+    exclude: [...configDefaults.exclude, 'dist/**'], // optional
   },
 });
